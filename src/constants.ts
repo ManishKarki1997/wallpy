d693c0edd3c872dca7dc859417bad8188681a333
Manish Karki <manishkarki247@gmail.com>
2024-06-09 22:53:01 +0545

    feat: add dotenv to read scrape variables from .env

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const SCRAPE_DETAIL_SLEEEP = 2000
 export const SCRAPE_URL_SLEEEP = 2000
-export const SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_TOPLIST = 2 // small value cuz the toplist won't be updated that frequently, currently only has 83 pages
-export const SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_LATEST = 100 // large because latest has like 19k+ pages
+export const SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_TOPLIST = process.env.SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_TOPLIST ? Number(process.env.SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_TOPLIST) : 5 // small value cuz the toplist won't be updated that frequently, currently only has 83 pages
+export const SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_LATEST = process.env.SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_LATEST ? Number(process.env.SCRAPE_TOTAL_PAGES_EACH_TIME_WALLHAVEN_LATEST) : 100 // large because latest has like 19k+ pages
 
 export const EVERY_6_HOURS = `0 */6 * * *`
 export const EVERY_4_HOURS = `0 */4 * * *`
