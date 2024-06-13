c3de852eadfee0cdb65d939b4bc1716037e45029
Manish Karki <manishkarki247@gmail.com>
2024-06-13 21:36:21 +0545

    update docker compose to setup postgres properly

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "postgresql://blackfeather247:blackfeather247@db:5432/wally_db?schema=public&connect_timeout=300" 
+      url: process.env.DATABASE_URL
     },
   },
+  // datasources: {
+  //   db: {
+  //     url: "postgresql://blackfeather247:blackfeather247@db:5432/wally_db?schema=public&connect_timeout=300" 
+  //   },
+  // },
 })
